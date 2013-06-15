007a5fe5e3f2aae3f5087aa64025009b7a219287
Mischa ter Smitten <mischa@tersmitten.nl>
2013-06-15 10:42:36 +0200

    PEP 8 style guide fixes

--- rtcom-eventlogger-export.py
+++ rtcom-eventlogger-export.py
@@ -15,7 +15,7 @@ if len(sys.argv) != 3:
   print ""
   sys.exit(1)
 
-dbFileName  = sys.argv[1].strip()
+dbFileName = sys.argv[1].strip()
 csvFileName = sys.argv[2].strip()
 
 if not os.path.exists(dbFileName):
@@ -27,15 +27,15 @@ if not os.path.exists(dbFileName):
 
 with sqlite3.connect(dbFileName) as connection, open(csvFileName, 'wb') as csvFd:
   cursor = connection.cursor()
-  writer = csv.writer(csvFd, delimiter = '\t', quotechar = '"', quoting = csv.QUOTE_ALL)
+  writer = csv.writer(csvFd, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
 
   sql = "SELECT start_time, remote_uid, free_text FROM Events WHERE event_type_id = 7"
   for record in cursor.execute(sql):
     startTime, phoneNumber, textMessage = record
 
-    startDateTime = datetime.fromtimestamp(startTime).strftime('%Y-%m-%d %H:%M:%S');
-    phoneNumber   = phoneNumber.encode('utf-8')
-    textMessage   = textMessage.encode('utf-8')
+    startDateTime = datetime.fromtimestamp(startTime).strftime('%Y-%m-%d %H:%M:%S')
+    phoneNumber = phoneNumber.encode('utf-8')
+    textMessage = textMessage.encode('utf-8')
 
     writer.writerow([startTime, startDateTime, phoneNumber, textMessage])
 
