947a2fba11a492ce5023e6d4142f259b41e6bcef
Mischa ter Smitten <mischa@tersmitten.nl>
2016-10-19 09:42:17 +0200

    Cs fixes

--- n900-sms-to-android-xml.py
+++ n900-sms-to-android-xml.py
@@ -3,13 +3,10 @@
 # -*- coding: utf-8 -*-
 
 """
-
 Reads the eventlogger database (el-v1.db) and writes the text messages
 to an XML file compatible with the backup format used by "Backup SMS
 Pro" on Android. Tested successfully importing 11,000+ SMS
 messages. Blatantly ripped off from rtcom-eventlogger-export.py...
-
-
 """
 
 import sys
@@ -39,7 +36,7 @@ try:
     cursor = connection.cursor()
 
     outFd.write("<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><backup><sms-backup>")
-    
+
     sql = 'SELECT `start_time`, `end_time`, `remote_uid`, `free_text` FROM `Events` WHERE `event_type_id` = 7'
     for record in cursor.execute(sql):
       startTime, endTime, phoneNumber, textMessage = record
@@ -52,35 +49,36 @@ try:
           msgtype = 2
         else:
           msgtype = 1
-        phoneNumber = string.replace(phoneNumber, "&", "&amp;")
-        phoneNumber = string.replace(phoneNumber, "\"", "&quot;")
-        phoneNumber = string.replace(phoneNumber, "\'", "&apos;")
-        phoneNumber = string.replace(phoneNumber, "<", "&lt;")
-        phoneNumber = string.replace(phoneNumber, ">", "&gt;")
-        phoneNumber = phoneNumber.encode('utf-8')
-        textMessage = string.replace(textMessage, "\r\n", " ") 
-        textMessage = string.replace(textMessage, "\n", " ")
-        textMessage = string.replace(textMessage, "&", "&amp;")
-        textMessage = string.replace(textMessage, "\"", "&quot;")
-        textMessage = string.replace(textMessage, "\'", "&apos;")
-        textMessage = string.replace(textMessage, "<", "&lt;")
-        textMessage = string.replace(textMessage, ">", "&gt;")
-        textMessage = textMessage.encode('utf-8')
-        outFd.write('<sms _id="%d">' % (count + offset))
-        outFd.write('<address>%s</address>' % phoneNumber)
-        outFd.write('<date>%d</date>' % (startTime * 1000))
-        outFd.write('<date_sent>%d</date_sent>' % 0)
-        outFd.write('<read>1</read>')
-        outFd.write('<status>-1</status>')
-        outFd.write('<type>%d</type>' % msgtype)
-        outFd.write('<body>%s</body>' % textMessage)
-        outFd.write('<seen>1</seen>')
-        outFd.write('</sms>')        
-        count += 1
-#        print "%d messages parsed" % count
+
+          phoneNumber = string.replace(phoneNumber, "&", "&amp;")
+          phoneNumber = string.replace(phoneNumber, "\"", "&quot;")
+          phoneNumber = string.replace(phoneNumber, "\'", "&apos;")
+          phoneNumber = string.replace(phoneNumber, "<", "&lt;")
+          phoneNumber = string.replace(phoneNumber, ">", "&gt;")
+          phoneNumber = phoneNumber.encode('utf-8')
+          textMessage = string.replace(textMessage, "\r\n", " ")
+          textMessage = string.replace(textMessage, "\n", " ")
+          textMessage = string.replace(textMessage, "&", "&amp;")
+          textMessage = string.replace(textMessage, "\"", "&quot;")
+          textMessage = string.replace(textMessage, "\'", "&apos;")
+          textMessage = string.replace(textMessage, "<", "&lt;")
+          textMessage = string.replace(textMessage, ">", "&gt;")
+          textMessage = textMessage.encode('utf-8')
+
+          outFd.write('<sms _id="%d">' % (count + offset))
+          outFd.write('<address>%s</address>' % phoneNumber)
+          outFd.write('<date>%d</date>' % (startTime * 1000))
+          outFd.write('<date_sent>%d</date_sent>' % 0)
+          outFd.write('<read>1</read>')
+          outFd.write('<status>-1</status>')
+          outFd.write('<type>%d</type>' % msgtype)
+          outFd.write('<body>%s</body>' % textMessage)
+          outFd.write('<seen>1</seen>')
+          outFd.write('</sms>')
+
+          count += 1
       except Exception, e:
         ignored += 1
-#        print "ignoring junk data:"
         pass
 
     outFd.write('</sms-backup></backup>\n')
