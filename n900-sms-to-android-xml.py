9d17cbbf7938f8c5990e38e405ad46beaf9be201
Will Thompson <will@willthompson.co.uk>
2017-04-17 10:43:59 +0100

    Rewrite XML export to use lxml, python3, ...
    It didn't work at all when I ran it because of some incorrect
    indentation; while fixing that I basically ended up rewriting it.

--- n900-sms-to-android-xml.py
+++ n900-sms-to-android-xml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
 
@@ -9,90 +9,86 @@ Pro" on Android. Tested successfully importing 11,000+ SMS
 messages. Blatantly ripped off from rtcom-eventlogger-export.py...
 """
 
-import sys
+import argparse
+from contextlib import closing
 import sqlite3
-import csv
-import string
-from datetime import datetime
+import logging
 
-import utils
-
-if len(sys.argv) != 3:
-  show_usage('%s [path to el-v1.db] [path to xml file]' % sys.argv[0])
-
-dbFileName = sys.argv[1].strip()
-xmlFileName = sys.argv[2].strip()
-
-try:
-  # Try to open eventlogger database
-  connection = sqlite3.connect(dbFileName)
-  # Try to open csv file for writing
-  outFd = open(xmlFileName, 'wb')
-  count = 0
-  offset = 1000
-  ignored = 0
-
-  with connection, outFd:
-    cursor = connection.cursor()
-
-    outFd.write("<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><backup><sms-backup>")
-
-    sql = 'SELECT `start_time`, `end_time`, `remote_uid`, `free_text` FROM `Events` WHERE `event_type_id` = 7'
-    for record in cursor.execute(sql):
-      startTime, endTime, phoneNumber, textMessage = record
-
-      startDateTime = datetime.fromtimestamp(startTime).strftime('%Y-%m-%d %H:%M:%SZ')
-      msgtype = 0
-
-      try:
-        if endTime > 0:
-          msgtype = 2
-        else:
-          msgtype = 1
-
-          phoneNumber = string.replace(phoneNumber, "&", "&amp;")
-          phoneNumber = string.replace(phoneNumber, "\"", "&quot;")
-          phoneNumber = string.replace(phoneNumber, "\'", "&apos;")
-          phoneNumber = string.replace(phoneNumber, "<", "&lt;")
-          phoneNumber = string.replace(phoneNumber, ">", "&gt;")
-          phoneNumber = phoneNumber.encode('utf-8')
-          textMessage = string.replace(textMessage, "\r\n", " ")
-          textMessage = string.replace(textMessage, "\n", " ")
-          textMessage = string.replace(textMessage, "&", "&amp;")
-          textMessage = string.replace(textMessage, "\"", "&quot;")
-          textMessage = string.replace(textMessage, "\'", "&apos;")
-          textMessage = string.replace(textMessage, "<", "&lt;")
-          textMessage = string.replace(textMessage, ">", "&gt;")
-          textMessage = textMessage.encode('utf-8')
-
-          outFd.write('<sms _id="%d">' % (count + offset))
-          outFd.write('<address>%s</address>' % phoneNumber)
-          outFd.write('<date>%d</date>' % (startTime * 1000))
-          outFd.write('<date_sent>%d</date_sent>' % 0)
-          outFd.write('<read>1</read>')
-          outFd.write('<status>-1</status>')
-          outFd.write('<type>%d</type>' % msgtype)
-          outFd.write('<body>%s</body>' % textMessage)
-          outFd.write('<seen>1</seen>')
-          outFd.write('</sms>')
-
-          count += 1
-      except Exception, e:
-        ignored += 1
-        pass
-
-    outFd.write('</sms-backup></backup>\n')
+from lxml import etree
+from lxml.builder import E
 
 
-  # Close database connection
-  connection.close()
+log = logging.getLogger(__name__)
 
-  # Close xml file
-  outFd.close()
-except sqlite3.Error, err:
-  utils.show_error('SQLite said: %s' % err)
-except IOError, err:
-  utils.show_error('Could not open xml file for writing: %s' % err)
 
-print "Read and wrote %d messages" % count
-print "Ignored %d junk messages" % ignored
+def format_record(record, id):
+    start_time, end_time, phone_number, text_message = record
+
+    # import datetime as dt
+    # start_date_time = dt.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%SZ')
+
+    if end_time > 0:
+        msgtype = 2
+    else:
+        msgtype = 1
+
+    return E.sms(
+        E.address(phone_number),
+        E.date(str(start_time * 1000)),
+        E.date_sent('0'),  # ???
+        E.read('1'),
+        E.status('-1'),  # ???
+        E.type(str(msgtype)),
+        E.body(text_message),
+        E.seen('1'),
+        _id=str(id),
+    )
+
+
+def main():
+    logging.basicConfig(level='INFO')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('database', help='path to el-v1.db')
+    parser.add_argument('xml', type=argparse.FileType(mode='wb'), help='path to XML file to write')
+    args = parser.parse_args()
+
+    count = 0
+    offset = 1000
+    ignored = 0
+
+    smss = []
+
+    # Try to open eventlogger database
+    with closing(sqlite3.connect(args.database)) as connection:
+        cursor = connection.cursor()
+
+        sql = '''
+        SELECT
+            `start_time`, `end_time`, `remote_uid`, `free_text`
+        FROM
+            `Events`
+        WHERE
+            `event_type_id` = 7
+        '''  # TODO: type 7 is only received, not sent, I think?
+        for record in cursor.execute(sql):
+            try:
+                smss.append(format_record(record, count + offset))
+                count += 1
+            except Exception:
+                log.exception('while processing record', *record)
+                ignored += 1
+
+    doc = E('backup', E('sms-backup', *smss))
+    args.xml.write(etree.tostring(doc,
+                                  encoding='utf-8',
+                                  xml_declaration=True,
+                                  standalone=True,
+                                  pretty_print=True))
+
+    print("Read and wrote %d messages" % count)
+    print("Ignored %d junk messages" % ignored)
+
+
+if __name__ == '__main__':
+    main()
