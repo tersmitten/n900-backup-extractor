0fd4a41f679835e8a5b865ff958afb2217fb6388
Will Thompson <will@willthompson.co.uk>
2017-04-17 12:25:11 +0100

    Fetch remote name too

--- n900-sms-to-android-xml.py
+++ n900-sms-to-android-xml.py
@@ -3,7 +3,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Reads the eventlogger database (el-v1.db) and writes the text messages
+Reads the eventlogger database (el.db) and writes the text messages
 to an XML file compatible with the backup format used by "Backup SMS
 Pro" on Android. Tested successfully importing 11,000+ SMS
 messages. Blatantly ripped off from rtcom-eventlogger-export.py...
@@ -21,28 +21,64 @@ from lxml.builder import E
 log = logging.getLogger(__name__)
 
 
-def format_record(record, id):
-    start_time, end_time, phone_number, text_message = record
+RTCOM_EL_EVENTTYPE_SMS_INBOUND = 7
+RTCOM_EL_EVENTTYPE_SMS_OUTBOUND = 8
 
+SMS_EVENT_TYPES = (
+    RTCOM_EL_EVENTTYPE_SMS_INBOUND,
+    RTCOM_EL_EVENTTYPE_SMS_OUTBOUND,
+)
+
+
+def format_record_xml(record, id):
+    '''
+    Backup format used by "Backup SMS Pro" on Android
+    '''
     # import datetime as dt
     # start_date_time = dt.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%SZ')
 
-    if end_time > 0:
+    if record['direction'] == 'Inbound':
         msgtype = 2
     else:
         msgtype = 1
 
     return E.sms(
-        E.address(phone_number),
-        E.date(str(start_time * 1000)),
+        E.address(record['remote_uid']),
+        E.date(str(record['start_time'] * 1000)),
         E.date_sent('0'),  # ???
         E.read('1'),
         E.status('-1'),  # ???
         E.type(str(msgtype)),
-        E.body(text_message),
+        E.body(record['free_text']),
         E.seen('1'),
         _id=str(id),
     )
+
+
+def format_xml(records):
+    count = 0
+    offset = 1000
+    ignored = 0
+
+    smss = []
+
+    for record in records:
+        try:
+            smss.append(format_record_xml(record, count + offset))
+            count += 1
+        except Exception:
+            log.exception('while processing record %s', record)
+            ignored += 1
+
+    print("Read and wrote %d messages" % count)
+    print("Ignored %d junk messages" % ignored)
+
+    doc = E('backup', E('sms-backup', *smss))
+    return etree.tostring(doc,
+                          encoding='utf-8',
+                          xml_declaration=True,
+                          standalone=True,
+                          pretty_print=True)
 
 
 def main():
@@ -50,44 +86,41 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('database', help='path to el-v1.db')
-    parser.add_argument('xml', type=argparse.FileType(mode='wb'), help='path to XML file to write')
+    parser.add_argument('output',
+                        type=argparse.FileType(mode='wb'),
+                        help='path to XML file to write')
     args = parser.parse_args()
-
-    count = 0
-    offset = 1000
-    ignored = 0
-
-    smss = []
 
     # Try to open eventlogger database
     with closing(sqlite3.connect(args.database)) as connection:
+        connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
 
+        # free_text is NULL when the SMS is a vCard
         sql = '''
         SELECT
-            `start_time`, `end_time`, `remote_uid`, `free_text`
+            CASE
+                WHEN e.`event_type_id` = ? THEN "Inbound"
+                WHEN e.`event_type_id` = ? THEN "Outbound"
+                ELSE NULL
+            END as `direction`,
+            e.`start_time`,
+            e.`remote_uid`,
+            r.`remote_name`,
+            e.`free_text`
         FROM
-            `Events`
+            `Events` e
+        LEFT JOIN
+            `Remotes` r
+        ON
+            e.`local_uid` = r.`local_uid` AND
+            e.`remote_uid` = r.`remote_uid`
         WHERE
-            `event_type_id` = 7
-        '''  # TODO: type 7 is only received, not sent, I think?
-        for record in cursor.execute(sql):
-            try:
-                smss.append(format_record(record, count + offset))
-                count += 1
-            except Exception:
-                log.exception('while processing record', *record)
-                ignored += 1
-
-    doc = E('backup', E('sms-backup', *smss))
-    args.xml.write(etree.tostring(doc,
-                                  encoding='utf-8',
-                                  xml_declaration=True,
-                                  standalone=True,
-                                  pretty_print=True))
-
-    print("Read and wrote %d messages" % count)
-    print("Ignored %d junk messages" % ignored)
+            e.`event_type_id` IN (?, ?) AND
+            e.`free_text` IS NOT NULL
+        '''
+        records = cursor.execute(sql, SMS_EVENT_TYPES * 2)
+        args.output.write(format_xml(records))
 
 
 if __name__ == '__main__':
